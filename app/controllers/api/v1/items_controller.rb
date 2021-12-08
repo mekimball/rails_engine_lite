051000873d76ccfbdbce1e70412b99787dbb83d5
Matthew Kimball <mekimba6@gmail.com>
2021-12-07 19:31:06 -0700

    adds delete to item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::ItemsController < ApplicationController
   def create
     item = Item.create(item_params)
     if item.save
-      render json: item
+      render json: item, status: 201
     end
   end
 
