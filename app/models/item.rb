015a5cded33279070c20c9e4cb5372fd4ef85855
Matthew Kimball <mekimba6@gmail.com>
2021-12-09 14:34:05 -0700

    changes limit(1) to .first on item and merchant search

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   def self.find_item(search_name)
-    where("name ILIKE ?", "%#{search_name}%").order('name asc').limit(1)
+    where("name ILIKE ?", "%#{search_name}%").order('name asc').first
   end
 
     def self.find_all_items(search_name)
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   has_many :items
 
     def self.find_merchant(search_name)
-    where("name ILIKE ?", "%#{search_name}%").order('name asc').limit(1)
+    where("name ILIKE ?", "%#{search_name}%").order('name asc').first
   end
 
     def self.find_all_merchants(search_name)
